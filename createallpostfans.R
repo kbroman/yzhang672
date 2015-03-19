62b990abbade6b9fb7fac2de477d1fb87aea9488
yzhang672 <yzhang@stat.wisc.edu>
2015-03-18 20:13:17 -0500

    added createallpostfans.R

--- createallpostfans.R
+++ createallpostfans.R
@@ -1,22 +1,5 @@
 library(Matrix)
 
-drop2 = function(tmp){
-  return(substr(tmp, start = 21, stop = nchar(tmp)))  
-}
-
-createA <- function(x,y)
-{
-  ux = unique(x); uy = unique(y);
-  n  = length(x); nx = length(ux); ny = length(uy)
-  cx = match(x,ux); cy = match(y,uy);
-  A  = spMatrix(nrow = nx , ncol = ny, 
-                i = cx , j = cy , x = rep(1,n))
-  
-  up = lapply(uy,drop2)
-  colnames(A) = up
-  return(A)
-}
-
 load('F:/UWMadison/project/French candidates/previous work/fordesk00/mydata/tables.RData')
 lepe = tables$lepen
 joly = tables$joly
